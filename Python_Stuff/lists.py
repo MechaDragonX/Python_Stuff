a648721ed0ccf173657a0f7889d4de72b40901a1
MechaDragonX <sonicraghav@gmail.com>
2019-03-04 11:08:35 -0800

    Initital commit

---
+++ Python_Stuff/lists.py
@@ -0,0 +1,3 @@
+things = ["thing", "thing1", "thing2"]
+a_thing = things[0]
+print(a_thing)
---
+++ Python_Stuff/simple_datatypes.py
@@ -0,0 +1,13 @@
+# Hello World 
+print("Hello World!")
+
+# Variables
+phrase = "This is not a phrase."
+x = 10
+lifesaver = 9.7
+buoy = float(237)
+print(phrase)
+
+#Adding
+sum = x + lifesaver + buoy
+print(sum)
